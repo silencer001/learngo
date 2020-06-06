16efa0460321366b81f5dc98de0c8b7edc3d5912
jude <judeng1990@gmail.com>
2020-06-06 17:53:25 +0800

    test

--- geeker/ch14/error/error_test.go
+++ geeker/ch14/error/error_test.go
@@ -6,10 +6,16 @@ import (
 	"errors"
 )
 
-func GetFibonacci(n int) ([]int, error) {
-	fibList := []int{1, 1}
+var TOO_SMALL_ERROR = errors.New("n should be bigger than 2")
+var TOO_BIG_ERROR = errors.New("n should be smaller than 50")
+
+func GetFibonacci(n int) ([]int64, error) {
+	fibList := []int64{1, 1}
 	if n < 2 {
-		return nil, errors.New("n should be in[2:]")
+		return nil, TOO_SMALL_ERROR
+	}
+	if n > 50 {
+		return nil, TOO_BIG_ERROR
 	}
 	for i := 2; i < n; i++ {
 		fibList = append(fibList, fibList[i-2]+fibList[i-1])
@@ -18,9 +24,14 @@ func GetFibonacci(n int) ([]int, error) {
 }
 
 func TestGetFibonacci(t *testing.T) {
-	s, err := GetFibonacci(-10)
+	s, err := GetFibonacci(100)
 	if err != nil {
-		t.Log(err)
+		if err == TOO_SMALL_ERROR {
+			t.Log("small error:", err)
+		}
+		if err == TOO_BIG_ERROR {
+			t.Log("big error: ", err)
+		}
 		return
 	}
 	t.Log(s)
