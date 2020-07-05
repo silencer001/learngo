4a66ad575749a622802e02abfbd2e1e12d800934
jude <judeng1990@gmail.com>
2020-07-06 07:37:47 +0800

    ...

--- geeker/ch28/reflect_test/reflect_test.go
+++ geeker/ch28/reflect_test/reflect_test.go
@@ -28,3 +28,29 @@ func TestBasicType(t *testing.T) {
 	var f float32 = 12
 	CheckType(&f)
 }
+
+type Employee struct {
+	EmployeeID string
+	Name       string `format:"normal"`
+	Age        int
+}
+
+func (e *Employee) UpdateAge(newVal int) {
+	e.Age = newVal
+}
+
+type Customer struct {
+	CookieID string
+	Name     string
+	Age      int
+}
+
+func TestInvokeByName(t *testing.T) {
+	e := &Employee{"1", "Mike", 30}
+	t.Logf("Name: value[%[1]v], Type(%[1]T)", reflect.ValueOf(*e).FieldByName("Name"))
+	if nameField, ok := reflect.Typeof(*e).FieldByName("Name"); !ok {
+		t.Error("Failed to get 'Name' field.")
+	} else {
+		t.Log("Tag:format", nameField.Tag.Get("format"))
+	}
+}
