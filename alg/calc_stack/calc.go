75be098e8062c08e03b5e9fdc387bbebea2db87b
judeng <judeng1990@gmail.com>
2021-01-31 14:58:58 +0800

    add myqueue

--- alg/calc_stack/calc.go
+++ alg/calc_stack/calc.go
@@ -99,3 +99,88 @@ func MustCalc(param1, param2 int, opera byte) int {
 		panic("unkown operator")
 	}
 }
+
+type Stack []int
+
+func (this *Stack) Push(x int) {
+	*this = append(*this, x)
+}
+func (this *Stack) Pop() int {
+	if len(*this) == 0 {
+		return 0
+	}
+	res := (*this)[len(*this)-1]
+	*this = (*this)[:len(*this)-1]
+	return res
+}
+func (this *Stack) Peek() int {
+	if len(*this) == 0 {
+		return 0
+	}
+	return (*this)[len(*this)-1]
+}
+func (this *Stack) IsEmpty() bool {
+	if len(*this) == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+type MyQueue struct {
+	istack Stack
+	ostack Stack
+}
+
+/** Initialize your data structure here. */
+func Constructor() MyQueue {
+	return MyQueue{
+		istack: []int{},
+		ostack: []int{},
+	}
+}
+
+/** Push element x to the back of queue. */
+func (this *MyQueue) Push(x int) {
+	this.istack.Push(x)
+}
+
+/** Removes the element from in front of queue and returns that element. */
+func (this *MyQueue) Pop() int {
+	if this.ostack.IsEmpty() {
+		for !this.istack.IsEmpty() {
+			x := this.istack.Pop()
+			this.ostack.Push(x)
+		}
+	}
+	return this.ostack.Pop()
+}
+
+/** Get the front element. */
+func (this *MyQueue) Peek() int {
+	if this.ostack.IsEmpty() {
+		for !this.istack.IsEmpty() {
+			x := this.istack.Pop()
+			this.ostack.Push(x)
+		}
+	}
+	return this.ostack.Peek()
+}
+
+/** Returns whether the queue is empty. */
+func (this *MyQueue) Empty() bool {
+	if this.ostack.IsEmpty() && this.istack.IsEmpty() {
+		return true
+	} else {
+		return false
+	}
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Peek();
+ * param_4 := obj.Empty();
+ */
