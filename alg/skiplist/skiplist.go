e27c9c9e5bd3278db610686a2e77a04db998a280
judeng <judeng1990@gmail.com>
2021-03-05 11:05:51 +0800

    find

--- alg/skiplist/skiplist.go
+++ alg/skiplist/skiplist.go
@@ -48,7 +48,7 @@ func NewSkipList() *SkipList {
 //辅助函数，返回1~32之间的整数
 func randomLevel() int {
 	level := 1
-	for rand.Int()%0xFFFF < (0xFFFF >> 2) { //1/4的概率
+	for (rand.Int() % 0xFFFF) < int(0xFFFF>>2) { //1/4的概率
 		level++
 	}
 	if level > SKIPLIST_MAX_LEVEL {
@@ -73,9 +73,12 @@ func (this *SkipList) SkipListInsert(score int64, ele string) {
 			if x.level[i].forward.score < score || (x.level[i].forward.score == score && x.level[i].forward.ele < ele) {
 				rank[i] += x.level[i].span
 				x = x.level[i].forward
+			} else {
+				break
 			}
 		}
 		update[i] = x //rank[i]是第i层x的排名
+		//fmt.Printf("debug:update[i]=%s\n", update[i].ele)
 	}
 	/*创建节点*/
 	myLevel := randomLevel()
@@ -83,7 +86,7 @@ func (this *SkipList) SkipListInsert(score int64, ele string) {
 	node := NewSkipListNode(myLevel, score, ele)
 	/*更新update和rank*/
 	if myLevel > this.level {
-		for i := myLevel; i < this.level; i++ {
+		for i := this.level; i < myLevel; i++ {
 			update[i] = this.header
 			rank[i] = 0
 			update[i].level[i].span = this.length
@@ -100,6 +103,10 @@ func (this *SkipList) SkipListInsert(score int64, ele string) {
 		node.level[i].span = update[i].level[i].span - (rank[0] - rank[i])
 		update[i].level[i].span = rank[0] - rank[i] + 1
 	}
+	/*myLevel以上的层级，span需要加1*/
+	for i := myLevel; i < this.level; i++ {
+		update[i].level[i].span++
+	}
 	/*更新backward指针*/
 	if update[0] == this.header {
 		node.backward = nil
@@ -113,14 +120,28 @@ func (this *SkipList) SkipListInsert(score int64, ele string) {
 	}
 
 	this.length++
-	this.DebugPrintSkipList()
-	fmt.Printf("\n\n")
+}
+func (this *SkipList) SkipListFindNode(score int64, ele string) *SkipListNode {
+	node := this.header
+	for i := this.level - 1; i >= 0; i-- {
+		for node.level[i].forward != nil {
+			fmt.Printf("node.level[i].forward.score:%d, ele:%s\n", node.level[i].forward.score, node.level[i].forward.ele)
+			if node.level[i].forward.score < score || (node.level[i].forward.score == score && node.level[i].forward.ele < ele) {
+				node = node.level[i].forward
+			} else if node.level[i].forward.score == score && node.level[i].forward.ele == ele {
+				return node
+			} else {
+				break
+			}
+		}
+	}
+	return nil
 }
 
 func (this *SkipList) DebugPrintSkipList() {
 	node := this.header
 	for node != nil {
-		fmt.Printf("level:%d,score=%d,ele=%s", len(node.level), node.score, node.ele)
+		fmt.Printf("level:%d,score=%d,ele=%s\n", len(node.level), node.score, node.ele)
 		for i := len(node.level) - 1; i >= 0; i-- {
 			var forward string
 			if node.level[i].forward == nil {
--- alg/skiplist/skiplist_test.go
+++ alg/skiplist/skiplist_test.go
@@ -1,6 +1,7 @@
 package skiplist
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -13,6 +14,42 @@ func TestInsert(t *testing.T) {
 	sl.SkipListInsert(3, "e")
 	sl.SkipListInsert(5, "f")
 	sl.SkipListInsert(4, "g")
+	sl.SkipListInsert(11, "h")
+	sl.SkipListInsert(18, "i")
+	sl.SkipListInsert(15, "j")
+	sl.SkipListInsert(30, "k")
+	sl.SkipListInsert(9, "l")
 	sl.DebugPrintSkipList()
 
+}
+func TestRandomLevel(t *testing.T) {
+	var count int
+	for i := 0; i < 20; i++ {
+		if randomLevel() > 1 {
+			count++
+		}
+	}
+	fmt.Println(count)
+}
+
+func TestSkipListFindNode(t *testing.T) {
+	sl := NewSkipList()
+	sl.SkipListInsert(1, "a")
+	sl.SkipListInsert(2, "b")
+	sl.SkipListInsert(10, "c")
+	sl.SkipListInsert(2, "d")
+	sl.SkipListInsert(3, "e")
+	sl.SkipListInsert(5, "f")
+	sl.SkipListInsert(4, "g")
+	sl.SkipListInsert(11, "h")
+	sl.SkipListInsert(18, "i")
+	sl.SkipListInsert(15, "j")
+	sl.SkipListInsert(30, "k")
+	sl.SkipListInsert(9, "l")
+	sl.DebugPrintSkipList()
+	if node := sl.SkipListFindNode(2, "d"); node != nil {
+		fmt.Println("found it!")
+	} else {
+		fmt.Println("not found")
+	}
 }
